18a6c1b16baec2ff6e58eb32f08ecb5868efe225
Ryan Blessing <ryanblessing20@yahoo.com>
2021-05-21 08:02:30 -0500

    updated coloring anf JS function to change colors '

--- assets/Java Script/moment.js
+++ assets/Java Script/moment.js
@@ -2,25 +2,29 @@
 var time = moment();
 //display time in the header ny selecting the id currentDay
 var displayMoment = document.getElementById("currentDay");
-//projecting time onto HTML and the type od dat format i want
+// format of moment i want to display
 displayMoment.innerHTML = time.format("MMMM Do YYYY, h:mm a");
-var nowTime = moment().hours()
+var nowTime = moment().hours();
 
 
 //time color changes for current-red, past-grey, future-green
+$(".hour").each(function() {
 
-$("hour").each(function() {
-
-    if (parseInt($(this).data < time)
-    )
-}
+    var nowTime = moment().hours();
 
     // parseInt turns a string to a integer
+    if (parseInt($(this).data("grey")) === nowTime) {
+        $(this).next().addClass("bg-danger")
+    }
+
+    if (parseInt($(this).data("grey")) < nowTime) {
+        $(this).next().addClass("bg-secondary")
+    }
+
+    if (parseInt($(this).data("grey")) > nowTime) {
+        $(this).next().addClass("big success")
+        }
+      });
+    
   
-
-
-
-
-
-
 //return and save into local storage
