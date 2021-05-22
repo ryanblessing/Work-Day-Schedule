cd29f6d1ad5a69c116c5e3ccb140d2912057cc23
Ryan Blessing <ryanblessing20@yahoo.com>
2021-05-22 09:40:54 -0500

    got time and color cordination for whether it is late, on the hour or in the future

--- assets/Java Script/moment.js
+++ assets/Java Script/moment.js
@@ -4,27 +4,61 @@ var time = moment();
 var displayMoment = document.getElementById("currentDay");
 // format of moment i want to display
 displayMoment.innerHTML = time.format("MMMM Do YYYY, h:mm a");
-var nowTime = moment().hours();
+var nowTime = moment().hours()
 
 
 //time color changes for current-red, past-grey, future-green
-$(".hour").each(function() {
+$(".hour").each(function () {
 
-    var nowTime = moment().hours();
+    var nowTime = moment().hours()
+    var currentTimeHourBlock = $(this).attr("data");
+    var textareaBlock = $(this).siblings("textarea");
+
 
     // parseInt turns a string to a integer
-    if (parseInt($(this).data("grey")) === nowTime) {
-        $(this).next().addClass("bg-danger")
+    if (parseInt(currentTimeHourBlock) < nowTime) {
+        $(this).addClass("bg-secondary")
+        textareaBlock.addClass("bg-secondary")
+
     }
 
-    if (parseInt($(this).data("grey")) < nowTime) {
-        $(this).next().addClass("bg-secondary")
+    if (parseInt(currentTimeHourBlock) === nowTime) {
+        $(this).addClass("bg-danger")
+        textareaBlock.addClass("bg-danger")
     }
 
-    if (parseInt($(this).data("grey")) > nowTime) {
-        $(this).next().addClass("big success")
-        }
-      });
-    
-  
+    if (parseInt(currentTimeHourBlock) > nowTime) {
+        $(this).addClass("bg-success")
+        textareaBlock.addClass("bg-success")
+    }
+
+})
+
+
 //return and save into local storage
+/*
+ var saveEvent = function() {
+
+     localStorage.setItem("text", json.stringify(saveEvent));
+     localStorage.getItem("text");
+
+ }
+
+ */
+var saveBtn = $('.save-btn');
+
+for (var i = 0; i < saveBtn.length; i++) {
+    saveBtn[i].addEventListener('click', function(){
+        var textAreaBlock = $(this).siblings("textarea").val();
+        var rowId = $(this).siblings("textarea").attr("id");
+        localStorage.setItem(rowId, textAreaBlock);
+    });
+    //                                            ^^^^^^^^^^^^^^^^^^^
+}
+
+for(var i = 1; i < 18; i++){
+    var textAreaInfo = localStorage.getItem(`row-${i}`)
+    $(`#row-${i}`).val(textAreaInfo);
+
+}
+
